87f0b33a303d69b82947cc7f2e5804b21de9b889
Lokesh Kumar <lokeshkumar797877@gmail.com>
2023-08-30 00:31:45 +0530

    further changes in the introduction file

---
+++ 01_Introduction/01_first.C
@@ -0,0 +1,13 @@
+# include<stdio.h>
+
+int main(){
+    /* This is a good program
+    you should keep it */
+    int lokesh ;
+//    This is a good program
+    int Lokesh ;
+    int error_code ;
+   // 'J' --> a character
+printf("Hello I am learning C with Harry");
+    return 0;
+}
---
+++ 01_Introduction/02_variables.c
@@ -0,0 +1,19 @@
+# include<stdio.h>
+
+int main(){
+    /* This is a good program
+    you should keep it */
+    int a = 4;
+   // int b = 8.5; // not recommended because it is a real no 
+   float b = 8.5;
+   char c = 'u';
+   int d = 45;
+   int e = 45 + 4;
+   printf("The value of a is %d \n " , a);
+   printf("The value of b is %f \n " , b);
+   printf("The value of a + d is %d \n ", e);
+   printf("The value of a + d is %d \n ", a + d);   
+   printf("The value of c is %c \n " , c);
+   printf("The values of a, b, c, d, e are %d %f %c %d %d \n" , a, b, c, d, e);
+    return 0;
+}
