c76b5d407ec69aff283844d214fd8ac8bd0dedec
Lokesh Kumar <lokeshkumar797877@gmail.com>
2023-09-11 23:03:07 +0530

    Initial Commit

---
+++ 04_loops_break_continue/01_Loop_intro.c
@@ -0,0 +1,22 @@
+# include<stdio.h>
+
+int main(){
+    int a=1;
+    printf("Hello ");
+    printf("World\n");
+    printf("%d\n", a);
+   // loops are used to repeat similar part of code snippet efficiently.
+    // a++;   100 times or more than as you want  
+    // printf("%d\n", a);
+    // a++;
+    // printf("%d\n", a);
+    // a++;
+    // printf("%d\n", a);
+    // a++;
+    // printf("%d\n", a);
+    // a++;
+    // printf("%d\n", a);
+    // a++;
+    // printf("%d\n", a);
+    return 0;
+}
