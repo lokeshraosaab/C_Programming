8e5bc81c822f453cd7d351179185e68787c6dd25
Lokesh Kumar <lokeshkumar797877@gmail.com>
2023-09-15 00:06:55 +0530

    Initial Commit

---
+++ 06_pointer/06_practice/01_problem_1.c
@@ -0,0 +1,12 @@
+# include<stdio.h>
+
+int main(){
+    int i = 4;
+    printf("The Value of the variable i is %d \n", i);
+    printf("The Address of the variable i is %u \n", &i);
+   // int *ptr;
+  //  ptr = &i;
+  //  printf("The Value of the variable i is %d \n", *ptr);
+ //   printf("The Address of the variable i is %u \n", ptr);
+    return 0;
+}
