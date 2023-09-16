36f40bf560dba1506581c654b063c3bc3e08d786
Lokesh Kumar <lokeshkumar797877@gmail.com>
2023-09-16 13:50:29 +0530

    Initial Commit

---
+++ 07_array/04_array_initialization.c
@@ -0,0 +1,18 @@
+# include<stdio.h>
+
+int main(){
+    int i[]= {34,53,63}; // int i[3]= {34,53,63}; i[3] or i[] both are correct
+    // if this type of declaration you performs.
+    printf("The value of i[0] is %d \n", i[0]);
+    printf("The value of i[1] is %d \n", i[1]);
+    printf("The value of i[2] is %d \n", i[2]);
+
+        float a[]= {3.4,5.3,63};
+    printf("The value of a[0] is %f \n", a[0]);
+    printf("The value of a[1] is %f \n", a[1]);
+    printf("The value of a[2] is %f \n", a[2]);
+
+
+
+    return 0;
+}
