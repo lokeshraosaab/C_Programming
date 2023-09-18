f1d328be5f8b42cae4ddd63ee5a5ae782c4acca7
Lokesh Kumar <lokeshkumar797877@gmail.com>
2023-09-18 15:31:37 +0530

    Initial Commit

---
+++ 08_string/01_basic_string.c
@@ -0,0 +1,9 @@
+# include<stdio.h>
+
+int main(){
+    char st[7] = {'L', 'o', 'k', 'e', 's', 'h', '\0'};
+    char str[7] = "Lokesh" ; // This is shortcut way for initialising strings . 
+    // In this case C adds a null character automatically.
+    return 0;
+}
+// These two are the ways for initialising strings. 
