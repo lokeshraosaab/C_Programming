62123d172ee3a51abc97710a2b562b27c58c5e74
Lokesh Kumar <lokeshkumar797877@gmail.com>
2023-09-19 18:48:58 +0530

    Initial Commit

---
+++ 08_string/08_practice/01_problem.c
@@ -0,0 +1,10 @@
+# include<stdio.h>
+
+int main(){
+   /* puts(); printf(); --> these are for printing 
+   scanf(); use by your machine for reading your inputs given by you by this 
+   but not multi-word string .
+   gets(); --> also use for multi-word strings for inputs and read by your machine. */
+
+    return 0;
+}
