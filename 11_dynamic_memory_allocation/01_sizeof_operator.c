fb1bf6b6fe011c78cc9251d86b749d902f9084cc
Lokesh Kumar <lokeshkumar797877@gmail.com>
2023-09-27 11:20:34 +0530

    Initial Commit

---
+++ 11_dynamic_memory_allocation/01_sizeof_operator.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("The size of the int on my pc is %d \n", sizeof(int));
+    printf("The size of the char on my pc is %d \n", sizeof(char));
+    printf("The size of the float on my pc is %d \n", sizeof(float));
+
+    return 0;
+}
