cb4460eacde983d789ad3e1a7a6e74d102c44235
Lokesh Kumar <lokeshkumar797877@gmail.com>
2023-09-28 08:42:31 +0530

    Initial Commit

---
+++ 11_dynamic_memory_allocation/11_practice/05_problem.c
@@ -0,0 +1,25 @@
+# include<stdio.h>
+# include<stdlib.h>
+
+int main(){
+    int *ptr;
+    ptr = (int*)malloc(10*sizeof(int));
+
+    for(int i=0; i<10; i++){
+        ptr[i] = 7*(i+1);
+    }
+    for(int i=0; i<10; i++){
+        printf("The value of element %d ptr[%d] is:- 7*%d = %d\n", i+1, i, i+1, ptr[i]);
+    }
+    
+    ptr = realloc(ptr, 15*sizeof(int));
+    printf("\n \n \n \n After reallocating ...\n \n \n ");
+     for(int i=0; i<15; i++){
+        ptr[i] = 7*(i+1);
+    }
+    for(int i=0; i<15; i++){
+        printf("The value of element %d ptr[%d] is:- 7*%d = %d\n", i+1, i, i+1, ptr[i]);
+    }
+    
+    return 0;
+}
