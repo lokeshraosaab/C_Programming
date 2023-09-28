99e63245460587abae417ff82175942dbf17f61d
Lokesh Kumar <lokeshkumar797877@gmail.com>
2023-09-28 08:42:55 +0530

    Initial Commit

---
+++ 11_dynamic_memory_allocation/11_practice/06_problem.c
@@ -0,0 +1,25 @@
+# include<stdio.h>
+# include<stdlib.h>
+
+int main(){
+    int *ptr;
+    ptr = (int*)calloc(5, sizeof(int));
+    for(int i=0; i<5; i++){
+        printf("Enter the value of element %d ptr[%d] :- ", i+1, i);
+        scanf("%d", &ptr[i]);
+    }
+    for(int i=0; i<5; i++){
+        printf("The value of element %d ptr[%d] is:- %d\n", i+1, i, ptr[i]);
+    }
+    
+    ptr = realloc(ptr, 10*sizeof(int));
+    
+     for(int i=0; i<10; i++){
+        printf("Enter the value of element %d ptr[%d] :- ", i+1, i);
+        scanf("%d", &ptr[i]);
+    }
+    for(int i=0; i<10; i++){
+        printf("The value of element %d ptr[%d] is:- %d\n", i+1, i, ptr[i]);
+    }
+    return 0;
+}
